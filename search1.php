cf89e4fb50f53f43c20c42a78fe1113528074ecb
thynpop <thayanee.pop@gmail.com>
2016-05-18 14:29:22 +0700

    modified search1 , search1Q

--- search1.php
+++ search1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Responsive Bootstrap Advance Admin Template</title>
+    <title>Music Ranking</title>
 
     <!-- BOOTSTRAP STYLES-->
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
@@ -18,157 +18,11 @@
 </head>
 <body>
     <div id="wrapper">
-        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">MERRY KING</a>
-            </div>
 
-            <div class="header-right">
-
-              <a href="message-task.html" class="btn btn-info" title="New Message"><b>30 </b><i class="fa fa-envelope-o fa-2x"></i></a>
-                <a href="message-task.html" class="btn btn-primary" title="New Task"><b>40 </b><i class="fa fa-bars fa-2x"></i></a>
-                <a href="login.html" class="btn btn-danger" title="Logout"><i class="fa fa-exclamation-circle fa-2x"></i></a>
-
-
-            </div>
-        </nav>
         <!-- /. NAV TOP  -->
         <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">
-                    <li>
-                        <div class="user-img-div">
-                            <center><img src="C:\xampp\htdocs\final\b.jpg" class="img-thumbnail" width="100" height="100" /></center>
 
-                        </div>
-                    </li>
-                    <li>
-                        <a  href="SpliceRecommendMenu.php"><i class="fa fa-dashboard "></i>Recommend menu</a>
-                    </li>
-                    <li>
-                    <a href="#"><i class="fa fa-edit "></i>Customer <span class="fa arrow"></span></a>
-                    <ul class="nav nav-second-level">
-
-
-                    <li>
-                        <a  href="HighCaloryReport.php"><i class="fa fa-dashboard "></i>Calory rank</a>
-                    </li>
-
-                    <li>
-                        <a href="OrderLineItem2.php"><i class="fa fa-yelp "></i>Order Here <span class="fa arrow"></span></a>
-                         <ul class="nav nav-second-level">
-                            <li>
-                                <a href="OrderLineItemtest2.php"><i class="fa fa-circle-o"></i>Add order</a>
-                            </li>
-                            <li>
-                                <a href="OrderLineItemReport.php"><i class="fa fa-circle-o"></i>Your order</a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a  href="Comment.html"><i class="fa fa-dashboard "></i>Comment</a>
-                    </li>
-                </ul>
-            </li>    
-
-
-
-
-
-
-                    <li>
-                        <a href="#"><i class="fa fa-edit "></i>Back End <span class="fa arrow"></span></a>
-                         <ul class="nav nav-second-level">
-                            <li>
-                                <a href="#"><i class="fa fa-yelp "></i>Menu <span class="fa arrow"></span></a>
-                                 <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="Menutest.php"><i class="fa fa-circle-o"></i>Add menu</a>
-                                    </li>
-                                    <li>
-                                        <a href="tbmenu.html"><i class="fa fa-circle-o"></i>Menu list</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            
-                            <li>
-                                <a href="listorder.html"><i class="fa fa-circle-o"></i>Update food status here</a>
-                            </li>
-                                
-                            <li>
-                                <a href="Invoice.html"><i class="fa fa-circle-o"></i>Check out</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-yelp "></i>Staff Register <span class="fa arrow"></span></a>
-                                 <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="staffregister.html"><i class="fa fa-circle-o"></i>Regis here</a>
-                                    </li>
-                                    <li>
-                                        <a href="tbstaff.html"><i class="fa fa-circle-o"></i>Staff list</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-
-                            <li>
-                                <a href="CirculationEachDayReport.php"><i class="fa fa-circle-o "></i>Circulation in each day</a>
-                            </li>
-                             <li>
-                                <a href="CommentReport.php"><i class="fa fa-circle-o "></i>All Comment</a>
-                            </li>
-                             <li>
-                                <a href="CustomerInDayReport.php"><i class="fa fa-circle-o "></i>Customer each day</a>
-                            </li>
-                             <li>
-                                <a href="MostUsedStockReport.php"><i class="fa fa-circle-o "></i>Most used ingredient</a>
-                            </li>
-                            <li>
-                                <a href="OftenComeReport.php"><i class="fa fa-circle-o "></i>Customer often come..people</a>
-                            </li>
-                             <li>
-                                <a href="OrderPerDayReport.php"><i class="fa fa-circle-o "></i>Order per day</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-yelp "></i>Promotion <span class="fa arrow"></span></a>
-                                 <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="Promotion.html"><i class="fa fa-circle-o"></i>Add promotion</a>
-                                    </li>
-                                    <li>
-                                        <a href="PromotionReport.php"><i class="fa fa-circle-o"></i>All promotion</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="RateReport.php"><i class="fa fa-circle-o "></i>Rank comment rate</a>
-                            </li>
-                            
-                            <li>
-                                <a href="#"><i class="fa fa-yelp "></i>Stock <span class="fa arrow"></span></a>
-                                 <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="Stocktest.php"><i class="fa fa-circle-o"></i>Add stock</a>
-                                    </li>
-                                    <li>
-                                        <a href="liststock.html"><i class="fa fa-circle-o"></i>Stock list</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                           
-                        </ul>
-                    </li>
-                     
-
-                        </ul>
                     </li>
                    
                     
@@ -177,12 +31,17 @@
             </div>
 
         </nav>
+
+
+
+
+
         <!-- /. NAV SIDE  -->
          <div id="page-wrapper">
             <div id="page-inner">
                 <div class="row">
                     <div class="col-md-12">
-                        <h1 class="page-head-line">Invoice</h1>
+                        <h1 class="page-head-line">SONG</h1>
                     </div>
                 </div>
                 <!-- /. ROW  -->
@@ -190,57 +49,34 @@
                     <div class="col-md-12">
                         <div class="panel panel-info">
 
-                            <div class="panel-heading">
-                             <label>Invoice report</label>
-                            </div>
-
-
-
+                           
                             <div class="panel-body">
-                                <!-- /. Splice edit php  -->
-                                <form id="songName" method="get" action="search1Q.php">
+                                <!-- link search1Q.php  -->
+                                <form id="songName" method="get" action="search1Q.php"> 
                                 <div class="row">
                                     <div class="col-md-4">
-                                        <label for="songName">Customer number</label>
-                                        <input type="text" class="form-control" id="songName" name="songName" placeholder="Enter customer number">
+                                        <label for="songName">SONG NAME</label> 
+                                        <!-- search box show the message "enter song name or singer name"-->
+                                        <input type="text" class="form-control" id="songName" name="songName" placeholder="Enter song name or singer name">
                                     </div>
                                 </div>
                                 </br>
                                 <div class="row">
                                     <div class="col-md-3">
-                                       <!--<button type="button" id="Query" class="btn btn-primary btn-block btn-lg">-->
-                                       <button type="submit" title="" class="add-author" id ="AddMore">Query</button>
+                                       <!--search button-->
+                                       <button type="submit" title="" class="add-author" id ="AddMore">SEARCH</button>
                                             
                                         </button>
                                     </div>
                                 </div>
                             </form>
 
-                            <div id="name">
 
-                            </div>
 
                                 </br>
 
 
-                             <div class="table-responsive">
-                                <table class="table table-bordered table-striped table-responsive" id="Recommend">
-                                    <thead>
-                                        <tr>
-                                            <th>Song Name</th>
-                                            <th>Singer</th>
-                                            <th>Vote</th>
-                                                
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        
-                                    </tbody>
-                                </table>
-                            </div>
-
-
-
+                    
 
 
                             </div>
@@ -249,21 +85,15 @@
                         </div>
                     </div>
                 </div>
-                <!-- /. sPLICE  -->
-            
-                
-                    <!-- /. ROW  -->
-            
+
             </div>
             <!-- /. PAGE INNER  -->
         </div>
         <!-- /. PAGE WRAPPER  -->
     </div>
     <!-- /. WRAPPER  -->
-    <div id="footer-sec">
-        &copy; 2014 YourCompany | Design By : <a href="http://www.binarytheme.com/" target="_blank">BinaryTheme.com</a>
-    </div>
-    <!-- /. FOOTER  -->
+ 
+
     <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
     <!-- JQUERY SCRIPTS -->
     <script src="assets/js/jquery-1.10.2.js"></script>
@@ -277,11 +107,7 @@
 
 </body>
 </html>
-<!-- Main Footer -->
-    <?php include("../footer.html");?>
 
-    <!-- Control Sidebar -->
-    <?php include("../controlbar.html");?>
 </div><!-- ./wrapper -->
 
 <!-- REQUIRED JS SCRIPTS -->
@@ -291,62 +117,7 @@
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/app.min.js"></script>
-<script>
 
-  
-
-
-
-$(document).ready(function() {
-
-        $("#ShowData").on("click", function ()) {
-            //$("#payment_table tbody > tr").remove();
-            var songName = new $('#songName').val();
-            
-
-           
-            loadTs();
-            function loadTs(songName){
-                //$("#payment_table tbody > tr").remove();
-                var xmlhttp = new XMLHttpRequest();
-                var url = "../musicRanking/search1Q.php";
-                
-                xmlhttp.onreadystatechange=function() {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        showdailyTime(xmlhttp.responseText);
-                    }
-                }
-                xmlhttp.open("GET", url, true);
-                xmlhttp.send();
-            }
-
-            function showdailyTime (response) {
-                console.log(response);
-                var arr = JSON.parse(response);
-                console.log(arr);
-                //var len = Object.keys(obj).length;
-                var i = 0;
-                var tds = "<tr>";
-            $("#Recommend tbody> tr").remove();
-
-                    console.log(arr.length);
-                    for (i = 0; i < arr.length; i++) {
-                        if (tds != "<tr>") {tds = "<tr>";}
-
-
-                       tds +=  "<td>" + arr[i].songName + "</td>";
-                        tds +=  "<td>" + arr[i].singerName + "</td>";
-                        tds +=  "<td>" + arr[i].vote + "</td>";
-                        console.log(tds);
-                    tds += "</tr>";
-                    $("#Recommend").append(tds);
-                        
-                    }
-
-            }
-        });
-    });
-</script>
 
 </body>
 </html>
@@ -354,5 +125,3 @@ $(document).ready(function() {
 
 
 
-mysqli_close($con);
-?>
--- search1Q.php
+++ search1Q.php
@@ -1,39 +1,77 @@
+<!-- create table-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Bootstrap Advance Admin Template</title>
 
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!--CUSTOM BASIC STYLES-->
+    <link href="assets/css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover" id="InvoiceTable"  style="border: 1px solid black; border-collapse: collapse;">
+        <thead>
+            <tr>
+                
+                <th>SONG</th>
+                <th>SINGER</th>
+                <th>VOTE</th>
+             
+            </tr>
+        </thead>
 
-
-
-
-
-
+<!-- query for seach song and singer-->
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+$connection = mysqli_connect("localhost","root","","music_ranking"); //connect to the music_ranking database
+$songName = $_GET["songName"]; //get input from search box
 
-$connection = mysqli_connect("localhost","root","","music_ranking");
-$songName = $_GET["songName"];
-
-
-
-$result2 = mysqli_query($connection, "SELECT s.songName , si.singerName , s.vote
-FROM singer si,song s
+//show song name, singer name and vote from singer and song table that songname like an input
+$result = mysqli_query($connection, "SELECT s.songName , si.singerName , s.vote
+FROM singer si, song s 
 WHERE s.singerID = si.singerID
-AND s.songName LIKE '$songName%'");
+AND s.songName LIKE '$songName%' ");
 
     $json = array();
-         while($current2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)) 
-             {
-            $json[] = array(
-         'songName' => $current2['songName'],
-             'singerName' => $current2['singerName'],
-            'vote' => $current2['vote']);
-                            }
+//loop for print the information       
+while($current = mysqli_fetch_assoc($result))
+    {
+        echo "<tr>";
+        echo "<td>" . $current["songName"] . "</td>" ;
+        echo "<td>" . $current["singerName"] . "</td>";
+        echo "<td>" . $current["vote"] . "</td>";
+        echo "</tr>";
+    }
+        echo json_encode($json);
+        mysqli_close($connection);
+//show song name, singer name and vote from singer and song table that singername like an input      
+$connection = mysqli_connect("localhost","root","","music_ranking");
+$result1 = mysqli_query($connection, "SELECT s.songName , si.singerName , s.vote
+FROM singer si,song s WHERE s.singerID = si.singerID
+AND si.singerName LIKE '$songName%'  ");
 
-                    echo json_encode($json);
-                    
-                    mysqli_close($connection);
-
-
-
-
+    $json = array();
+//loop for print the information       
+while($current1 = mysqli_fetch_assoc($result1))
+    {
+        echo "<tr>";
+        echo "<td>" . $current1["songName"] . "</td>" ;
+        echo "<td>" . $current1["singerName"] . "</td>";
+        echo "<td>" . $current1["vote"] . "</td>";
+        echo "</tr>";
+    }
+        echo json_encode($json);
+        mysqli_close($connection);
 ?>
-                                        
+
+
+    </table>
+</div>
+</html>
