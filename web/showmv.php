b571a1d90697b2fe5d029346ca3c383d0cf8be05
thynpop <thayanee.pop@gmail.com>
2016-05-19 13:44:01 +0700

    link song with video youtube id

--- web/showmv.php
+++ web/showmv.php
@@ -1,12 +1,16 @@
 <?php
-  $data = '6hH9m1G9f10';
+$connection = mysqli_connect("localhost","root","","music_ranking");
+  $data = $_GET["data"];
+
 
   echo '<script type="text/javascript">';
   echo "var data = '$data';"; 
   echo '</script>';
+
+
+
 ?>
 <div id ="ytplayer"></div>
-
 <script type="text/javascript" >
 
 console.log(data)
@@ -24,4 +28,16 @@ console.log(data)
 
 
 </script>
+<?php
+$connection = mysqli_connect("localhost","root","","music_ranking");
+$result1 = mysqli_query($connection, "SELECT s.songName , si.singerName , s.vote
+            FROM singer si,song s WHERE s.singerID = si.singerID
+            AND s.link = '$data' ");
+$current1 = mysqli_fetch_assoc($result1);
 
+
+        echo '<p><h5> '.$current1['songName'].' </h5></p>';
+        echo '<p><h5> '.$current1['singerName'].' </h5></p>';
+        echo '<p><h5>VOTE : '.$current1['vote'].' </h5></p>';
+mysqli_close($connection);
+?>
